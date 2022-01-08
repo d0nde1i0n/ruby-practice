b4e08f7c2054874412284da2c66048927b942b77
EC2 Default User <ec2-user@ip-172-31-3-84.ec2.internal>
2022-01-08 07:31:50 +0000

    [End]　2-6 演習問題終了

---
+++ lesson6.rb
@@ -0,0 +1,13 @@
+
+total_price = 100
+
+if total_price > 100
+  puts "みかんを購入。所持金に余りあり。"
+elsif total_price == 100
+  puts "みかんを購入。所持金0円"
+elsif toral_price >= 0 && total_price < 100
+  puts "みかんを購入することはできません。"
+else
+  pust "入力値エラー"
+end
+
